cf7c1895d22d360d76195e735b5e099d48569de3
Simeon Minot <sminot@z1r8p5.42lyon.fr>
2025-03-24 20:06:05 +0100

    end

--- include/cube.h
+++ include/cube.h
@@ -6,7 +6,7 @@
 /*   By: sminot <simeon.minot@outlook.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 17:35:44 by sminot            #+#    #+#             */
-/*   Updated: 2025/03/24 18:39:48 by sminot           ###   ########.fr       */
+/*   Updated: 2025/03/24 20:01:11 by sminot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,34 @@
 
 # include <fcntl.h>
 
+typedef enum e_bool
+{
+	FALSE ,
+	TRUE
+}	t_bool;
+
 typedef struct s_map
 {
-	int		x_max;
-	int		y_max;
-	char	**map;
+	unsigned long	x_max;
+	unsigned long	y_max;
+	char			**map;
 }	t_map;
 
 typedef struct s_data
 {
 	t_map	*map;
 }	t_data;
+
+/*---------------------------------------------------------------------------*/
+/*                            parse.c                                        */
+/*---------------------------------------------------------------------------*/
+typedef struct s_reading_map_status
+{
+	t_bool	map_start;
+	t_bool	map_is_end;
+	t_bool	param_start;
+	t_bool	param_is_end;
+}	t_reading_map_status;
 
 void	parse_map(char *map_name, t_map *map);
 
--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,30 +6,120 @@
 /*   By: sminot <simeon.minot@outlook.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 18:00:31 by sminot            #+#    #+#             */
-/*   Updated: 2025/03/24 18:40:35 by sminot           ###   ########.fr       */
+/*   Updated: 2025/03/24 20:05:51 by sminot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
 
-void	parse_map(char *map_name, t_map *map)
+t_bool	map_invalid(t_map *map, char *line)
+{
+	//free (status) + liste chainer aussi
+(void)map;
+(void)line;
+exit(EXIT_FAILURE);
+}
+
+static t_bool	is_color_texture_or_empty(char *line)
+{
+	size_t i;
+
+	i = -1;
+	while (line[++i] == ' ')
+		;
+	if (line[i] == '\0')		//si on n'accepte pas les ligne avec que des espace
+		return (TRUE);			//mettre cette condition avant de passer les espaces
+	if (ft_strncmp(&line[i], "NO", 2) == 0)
+		return (TRUE);
+	if (ft_strncmp(&line[i], "SO", 2) == 0)
+		return (TRUE);
+	if (ft_strncmp(&line[i], "WE", 2) == 0)
+		return (TRUE);
+	if (ft_strncmp(&line[i], "EA", 2) == 0)
+		return (TRUE);
+	if (ft_strncmp(&line[i], "F", 1) == 0)
+		return (TRUE);
+	if (ft_strncmp(&line[i], "C", 1) == 0)
+		return (TRUE);
+	return (FALSE);
+}
+
+/**
+ * @note set the reading status to manage if map and other information is mixed
+ * and exit if necessery
+ */
+static t_bool	is_a_map_line(char *line, t_reading_map_status *status, \
+							t_map *map)
+{
+	if (is_color_texture_or_empty(line) == TRUE)
+	{
+		status->param_start = TRUE;
+		if (status->param_is_end == TRUE)
+			return (map_invalid(map, line));
+		if (status->map_start)
+			status->map_is_end = TRUE;
+		return (FALSE);
+	}
+	else
+	{
+		status->map_start = TRUE;
+		if (status->map_is_end == TRUE)
+			return (map_invalid(map, line));
+		if (status->param_start)
+			status->param_is_end = TRUE;
+		status->map_start = TRUE;
+		return (TRUE);
+	}
+}
+
+static void	set_x_and_y_max(int fd, t_map *map)
+{
+	char					*line;
+	t_reading_map_status	*status;
+
+	status = ft_calloc(1, sizeof(status));
+	line = get_next_line(fd);
+	map->x_max = ft_strlen(line) - 2;
+	while (line)
+	{
+		if (is_a_map_line(line, status, map))
+			;
+			//creer une liste chainer avec les lignes
+		if (map->x_max < ft_strlen(line) - 2)
+			map->x_max = ft_strlen(line) - 2;
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	--(map->y_max);
+	free(status);
+}
+
+static void	creat_and_fill_map(char *map_name, t_map *map)
 {
 	int	fd;
 
-	//creat_map(map_name, map);
 	fd = open(map_name, O_RDONLY);
 	if (fd == -1)
 	{
 		putstr_fd("Error\nMap not open\n", 2);
-		//free_map(map);
+		free(map);
 		exit(EXIT_FAILURE);
 	}
-	//fill_map(fd, map);
+	set_x_and_y_max(fd, map);			//recupere la liste chainer + changer le nom de la fonciton
 	if (close(fd))
 	{
 		putstr_fd("Error\nMap refuse to close\n", 2);
-		//free_map(map);
+		free(map);
 		exit(EXIT_FAILURE);
 	}
-	(void)map;
+	//dup chaque element de lise pour avoir une map rectangle 
+	//					-> completer par des espaces ' ' les lignes plus courtes;
+}
+
+void	parse_map(char *map_name, t_map *map)
+{
+
+	creat_and_fill_map(map_name, map);
+	//check_map(map)
 }
